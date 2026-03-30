81904b21f7ea6cd45dea69c8eb91134268fc4ef4
BolBob <tranvinh251007@gmail.com>
2026-03-30 22:59:03 +0700

    hom nay la ngay 9/11 lan 2

---
+++ src/ITITIU25044_TrinhTranVinh_Lab03/Problem1_2/Animal.java
@@ -0,0 +1,14 @@
+package ITITIU25044_TrinhTranVinh_Lab03.Problem1_2;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
