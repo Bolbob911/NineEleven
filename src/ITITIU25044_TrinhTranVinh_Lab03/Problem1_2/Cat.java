0818194295da8a5924b9cd2d08d171e3fca90bda
BolBob <tranvinh251007@gmail.com>
2026-03-30 23:16:58 +0700

    d

---
+++ src/ITITIU25044_TrinhTranVinh_Lab03/Problem1_2/Cat.java
@@ -0,0 +1,14 @@
+package ITITIU25044_TrinhTranVinh_Lab03.Problem1_2;
+
+public class Cat extends Mammal {
+    public Cat(String name) {
+        super(name);
+    }
+    public void greets() {
+        System.out.println("Meow");
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
